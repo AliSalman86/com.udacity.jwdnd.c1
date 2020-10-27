687e0e5f8cfe68c0dc55ee5148e135da96f4e369
AliSalman86 <eng.aliabdullah@gmail.com>
2020-10-26 22:48:15 -0500

    Adding first anitations: @Configuration, @EnableAutoConfiguration, @Bean, @Primary

--- src/main/java/com/udacity/jwdnd/c1/review/ReviewApplication.java
+++ src/main/java/com/udacity/jwdnd/c1/review/ReviewApplication.java
@@ -1,9 +1,15 @@
 package com.udacity.jwdnd.c1.review;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
 public class ReviewApplication {
 
 	public static void main(String[] args) {
@@ -11,4 +17,20 @@ public class ReviewApplication {
 		System.out.println("Hello Spring");
 	}
 
+	@Bean
+	public String basicMessage() {
+		System.out.println("inside basicMessage");
+		return "Hello";
+	}
+	@Primary
+	@Bean
+	public String componundMessage(String basicMessage) {
+		System.out.println("inside compoundMessage, received: " + basicMessage);
+		return basicMessage + ", Spring";
+	}
+	@Bean
+	public int characterCount(String message) {
+		System.out.println(message.length());
+		return message.length();
+	}
 }
